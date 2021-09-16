f68e33309110f48f3f14c4bc590eb5d1a2e154c5
AlishGrg <alish.grg04@gmail.com>
2021-09-16 16:48:28 +0530

    first commit

---
+++ MavenJenkin/src/main/java/com/lti/jenk/MyCalc.java
@@ -0,0 +1,12 @@
+package com.lti.jenk;
+
+public class MyCalc {
+
+	public int getSum(int x,int y) {
+		return x+y;
+	}
+	public int getDiff(int x,int y) {
+		return x-y;
+	}
+}
+
---
+++ MavenJenkin/src/test/java/com/lti/jenk/MyCalcTest.java
@@ -0,0 +1,26 @@
+package com.lti.jenk;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class MyCalcTest {
+
+	@Test
+	public void test1() {
+		assertEquals(25,new MyCalc().getSum(10, 15));
+	}
+	@Test
+	public void test2() {
+		assertEquals(40,new MyCalc().getSum(25, 15));
+	}
+	@Test
+	public void test3() {
+		assertEquals(5,new MyCalc().getDiff(15, 10));
+	}
+	@Test
+	public void test4() {
+		assertEquals(20,new MyCalc().getSum(22, 2));
+	}
+
+}
